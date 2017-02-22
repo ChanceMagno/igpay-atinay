0025bd9be3503100600e459c7ff78562feebbba5
Chance magno and Ryan Jones <ryan@>
2017-02-22 10:18:45 -0800

    add directories, single vowel logic

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+
+var string = "u";
+console.log(string);
+
+if (string === "a" || string === "e" || string === "i" || string === "o" || string === "u") {
+	string = string + "ay";
+}
+
+console.log(string);

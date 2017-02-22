34d9e90440df524aa2ea7bc7c8e527defd94bc95
Chance magno and Ryan Jones <chance@>
2017-02-22 13:58:55 -0800

    identify qu in a sentence and return proper piglatin-still needs to check for vowels before finding Q.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 /********* First Test Case *********/
 /* This will add ay to the end of a single vowel */
 
-// var string = "u";
-// console.log(string);
-//
-// if (string === "a" || string === "e" || string === "i" || string === "o" || string === "u") {
-// 	string = string + "ay";
-// }
-//
-// console.log(string);
+var string = "u";
+console.log(string);
+
+if (string === "a" || string === "e" || string === "i" || string === "o" || string === "u") {
+	string = string + "ay";
+}
+
+console.log(string);
 
 
 /********* Second Test Case *********/
@@ -32,7 +32,7 @@ console.log(res);
 /* This will take a word with a single vowel at the start and add ay to the end */
 /* This will take a string of multiple constanants and save them in an array until it hits a vowel, then take the word starting at the vowel and add the constanants before the vowel plus ay. */
 
-var string = "eipple";
+var string = "strap";
 var res = "";
 var array = [];
 var pigLatin = "";
@@ -54,3 +54,25 @@ pigLatin = res + constants + "ay"; // ap + str + ay
 console.log("Pig Latin, " + pigLatin);
 console.log("Our constants " + constants);
 console.log("Our Array: " + array);
+
+
+/********* Third Test Case *********/
+//this will identify qu in a sentence and return proper piglatin-still needs to check for vowels before finding Q. 
+
+var string = "quinn";
+var quEnd = "";
+var qu = "";
+console.log(qu);
+var finalString = "";
+
+for(i = 0; i < string.length; i++) {
+	if (string.charAt(i) == "q") {
+		if(string.charAt(i+1) == "u") {
+    	quEnd = string.slice(i+2, string.length);
+      qu = string.charAt(i) + string.charAt(i+1);
+   	}
+  }
+ }
+
+finalString = quEnd + qu + "ay";
+console.log(finalString);

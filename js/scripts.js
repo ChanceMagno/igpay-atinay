926c56ad852550d3b09e5e4fb3189d44f65b4e0d
Chance magno and Ryan Jones <ryan@>
2017-02-22 11:11:34 -0800

    add logic for single constantant and vowel words

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
+/********* First Test Case *********/
+/* This will add ay to the end of a single vowel */
 
-var string = "u";
+// var string = "u";
+// console.log(string);
+//
+// if (string === "a" || string === "e" || string === "i" || string === "o" || string === "u") {
+// 	string = string + "ay";
+// }
+//
+// console.log(string);
+
+
+/********* Second Test Case *********/
+/* This will take the first constant and put it at the end + ay */
+/* This will take a word with a single vowel at the start and add ay to the end */
+
+var string = "barbeque";
+var res = "";
 console.log(string);
 
-if (string === "a" || string === "e" || string === "i" || string === "o" || string === "u") {
-	string = string + "ay";
+if (string.charAt(0) !== "a" && string.charAt(0) !== "e" && string.charAt(0) !== "i" && string.charAt(0) !== "o" && string.charAt(0) !== "u") {
+	res = string.slice(1, string.length); // res = at
+	res = res + string.charAt(0) + "ay"; // at + c + ay = atcay
+} else {
+	res = string + "ay";
 }
-
-console.log(string);
+console.log(res);
